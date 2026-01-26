5b30788cfd4beea878cf1b0c9d1db3b8df4bbadf
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-01-26 22:10:38 +0100

    add spacing system and typography foundations

---
+++ composeApp/src/androidMain/kotlin/com/github/eylulnc/notia/ui/theme/Spacing.kt
@@ -0,0 +1,24 @@
+package com.github.eylulnc.notia.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+object Spacing {
+    val xs = 4.dp
+    val s = 8.dp
+    val m = 12.dp
+    val l = 16.dp
+    val xl = 24.dp
+    val xxl = 32.dp
+
+
+    val letterWide = 1.5.sp
+
+}
+
+object FontSizes {
+    val caption = 12.sp
+    val body = 16.sp
+    val button = 18.sp
+    val title = 32.sp
+}
