3a15a2e7e41957590d3debf7dc599179e09be256
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-01-26 22:12:28 +0100

    update module and create TodayViewModel

--- composeApp/src/androidMain/kotlin/com/github/eylulnc/notia/di/AppModule.kt
+++ composeApp/src/androidMain/kotlin/com/github/eylulnc/notia/di/AppModule.kt
@@ -1,7 +1,14 @@
 package com.github.eylulnc.notia.di
 
+import com.github.eylulnc.notia.data.FocusRepositoryImpl
+import com.github.eylulnc.notia.data.storage.FocusStorage
+import com.github.eylulnc.notia.domain.repository.FocusRepository
+import com.github.eylulnc.notia.storage.AndroidFocusStorage
+import com.github.eylulnc.notia.ui.today.TodayViewModel
 import com.github.eylulnc.notia.util.DateProvider
 import com.github.eylulnc.notia.util.SystemDateProvider
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -9,4 +16,23 @@ val appModule = module {
     single<DateProvider> {
         SystemDateProvider()
     }
+
+    single<FocusStorage> {
+        AndroidFocusStorage(androidContext())
+    }
+
+    single<FocusRepository> {
+        FocusRepositoryImpl(
+            storage = get(),
+            dateProvider = get()
+        )
+    }
+
+    viewModel {
+        TodayViewModel(
+            focusRepository = get(),
+            dateProvider = get()
+        )
+    }
 }
+
---
+++ composeApp/src/androidMain/kotlin/com/github/eylulnc/notia/ui/today/TodayViewModel.kt
@@ -0,0 +1,59 @@
+package com.github.eylulnc.notia.ui.today
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.eylulnc.notia.domain.repository.FocusRepository
+import com.github.eylulnc.notia.util.DateProvider
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class TodayViewModel(
+    private val focusRepository: FocusRepository,
+    private val dateProvider: DateProvider
+) : ViewModel() {
+
+    private val isEditing = MutableStateFlow(false)
+
+    val uiState = combine(
+        focusRepository.getTodayFocus(),
+        focusRepository.getCurrentStreak(),
+        isEditing
+    ) { focus, streak, editing ->
+        val today = dateProvider.today()
+
+        TodayUiState(
+            dateLabel = today.dayOfWeek.name,
+            focusText = focus?.text,
+            currentStreak = streak,
+            isEditing = editing
+        )
+    }.stateIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(5_000),
+        TodayUiState("", null, 0)
+    )
+
+    fun startEditing() {
+        isEditing.value = true
+    }
+
+    fun cancelEditing() {
+        isEditing.value = false
+    }
+
+    fun saveFocus(text: String) {
+        viewModelScope.launch {
+            focusRepository.setTodayFocus(text)
+            isEditing.value = false
+        }
+    }
+
+    fun clearFocus() {
+        viewModelScope.launch {
+            focusRepository.deleteTodayFocus()
+        }
+    }
+}
