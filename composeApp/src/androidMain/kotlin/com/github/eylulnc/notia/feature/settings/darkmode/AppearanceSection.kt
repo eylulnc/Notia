da7033bbc897f5e0d60d04bb1ea628b366614eff
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-01-27 13:17:06 +0100

    implement appearance theme toggle

---
+++ composeApp/src/androidMain/kotlin/com/github/eylulnc/notia/feature/settings/darkmode/AppearanceSection.kt
@@ -0,0 +1,28 @@
+package com.github.eylulnc.notia.feature.settings.darkmode
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.eylulnc.notia.R
+import com.github.eylulnc.notia.feature.settings.common.SettingsGroup
+import com.github.eylulnc.notia.ui.theme.ThemeMode
+
+@Composable
+fun AppearanceSection(
+    selected: ThemeMode,
+    onSelect: (ThemeMode) -> Unit
+) {
+    SettingsGroup(title = stringResource(R.string.settings_appearance)) {
+        SegmentedControl(
+            options = ThemeMode.entries,
+            selected = selected,
+            onSelect = onSelect
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AppearanceSectionPreview() {
+    AppearanceSection(ThemeMode.LIGHT, {})
+}
