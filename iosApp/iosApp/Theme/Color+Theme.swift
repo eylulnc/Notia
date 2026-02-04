6667366004c49708322f863fc214c240590cfb2a
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-02-04 21:37:18 +0100

    add colors to assets

---
+++ iosApp/iosApp/Theme/Color+Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Theme.swift
+//  iosApp
+//
+//  Created by Eylul Naz Can on 4.02.2026.
+//
+
+import SwiftUI
+
+extension Color {
+    static let notiaBackground = Color("NotiaBackground")
+    static let notiaSurface = Color("NotiaSurface")
+    static let notiaText = Color("NotiaText")
+    static let notiaPrimary = Color("NotiaPrimary")
+    static let notiaAccent = Color("NotiaAccent")
+    static let notiaSurfaceVariant = Color("NotiaSurfaceVariant")
+    static let notiaOutline = Color("NotiaOutline")
+}
