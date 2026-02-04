e00a79951dbe4446a6a16bb2bff67ec1a5b721d2
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-02-04 21:40:04 +0100

    create Spacing and Fontsize

---
+++ iosApp/iosApp/Theme/NotiaFontSizes.swift
@@ -0,0 +1,20 @@
+//
+//  NotiaFontSizes.swift
+//  iosApp
+//
+//  Created by Eylul Naz Can on 4.02.2026.
+//
+
+import CoreFoundation
+
+struct NotiaFontSizes {
+    static let caption: CGFloat = 12
+    static let label: CGFloat = 14
+    static let body: CGFloat = 16
+    static let button: CGFloat = 18
+    static let bodyLarge: CGFloat = 18
+    static let subTitle: CGFloat = 24
+    static let midTitle: CGFloat = 28
+    static let title: CGFloat = 32
+    static let largeTitle: CGFloat = 36
+}
