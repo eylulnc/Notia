fd5b6b7170cc297061148150c4a30cb36074ea3c
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-03-01 16:23:12 +0100

    create Util to handle flow and manage inject on KoinHelper

--- shared/src/commonMain/kotlin/com/github/eylulnc/notia/di/KoinHelper.kt
+++ shared/src/commonMain/kotlin/com/github/eylulnc/notia/di/KoinHelper.kt
@@ -1,6 +1,17 @@
 package com.github.eylulnc.notia.di
 
-class KoinHelper {
+import com.github.eylulnc.notia.domain.repository.FocusRepository
+import com.github.eylulnc.notia.domain.repository.ReminderRepository
+import com.github.eylulnc.notia.util.DateProvider
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class KoinHelper : KoinComponent {
+    
+    val focusRepository: FocusRepository by inject()
+    val reminderRepository: ReminderRepository by inject()
+    val dateProvider: DateProvider by inject()
+
     fun iOSInit() {
         KoinStarter.startIOS()
     }
