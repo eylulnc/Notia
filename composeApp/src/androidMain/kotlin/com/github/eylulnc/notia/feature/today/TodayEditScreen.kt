22d2eaf6ba47272a444c578fd77429d6ed69ff31
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-03-30 16:33:00 +0200

    clean imports

--- composeApp/src/androidMain/kotlin/com/github/eylulnc/notia/feature/today/TodayEditScreen.kt
+++ composeApp/src/androidMain/kotlin/com/github/eylulnc/notia/feature/today/TodayEditScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -21,7 +20,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
@@ -32,6 +30,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.eylulnc.notia.R
 import com.github.eylulnc.notia.ui.common.NotiaSecondaryButton
 import com.github.eylulnc.notia.ui.theme.FontSizes
