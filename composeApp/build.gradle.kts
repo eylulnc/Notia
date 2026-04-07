561de8be5dafc2411d60d2c896ddbf739fce5ae2
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-04-07 12:15:15 +0200

    fix version display

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -60,6 +60,9 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    buildFeatures {
+        buildConfig = true
+    }
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
